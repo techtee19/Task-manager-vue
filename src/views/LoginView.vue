ad7f56b8a5a9a2167e556ebe455df2d00265d841
techtee19 <techtee19@gmail.com>
2025-06-03 19:50:09 +0100

    fix: correct casing for LoginForm component import and usage in LoginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -13,7 +13,7 @@
         </div>
 
         <!-- Login Card -->
-        <login-form
+        <LoginForm
           @login="handleLogin"
           @signup="handleSignup"
           :loading="loading"
@@ -36,8 +36,7 @@
 <script>
 import { mapState, mapActions } from 'pinia'
 import { useAuthStore } from '@/stores/auth'
-import LoginForm from '@/components/auth/loginForm.vue'
-
+import LoginForm from '@/components/auth/LoginForm.vue'
 export default {
   name: 'LoginPage',
   components: {
